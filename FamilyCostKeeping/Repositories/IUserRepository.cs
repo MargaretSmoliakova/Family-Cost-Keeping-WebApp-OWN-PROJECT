937b15d793243c2ea7b171d7691aaf9abe3a91bb
Margarita Smoliakova <msmoliakova@servicechannel.com>
2017-10-26 02:10:39 +0300

    repo User

---
+++ FamilyCostKeeping/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using FamilyCostKeeping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyCostKeeping.Repositories
+{
+    interface IUserRepository : IRepository<User, int>
+    {
+        
+    }
+}
---
+++ FamilyCostKeeping/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FamilyCostKeeping.Models;
+using FamilyCostKeeping.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyCostKeeping.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly FamilyCostKeepingDbContext _dbContext;
+
+        private DbSet<User> Users
+        {
+            get
+            {
+                return _dbContext.Users;
+            }
+        }
+
+    public UserRepository(FamilyCostKeepingDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Create(User entity)
+        {
+            Users?.Add(entity);
+            Save();
+        }
+
+        public void Delete(User entity)
+        {
+            _dbContext.Users.Remove(entity);
+            Save();
+        }
+
+        public User GetById(int id)
+        {
+            return _dbContext.Users.Select(u => u).Where(u => u.UserId == id).FirstOrDefault();
+        }        
+
+        public void Update(User entity)
+        {
+            _dbContext.en.Users.Select(u => u).Where(u => u.UserId == entity.UserId).
+        }
+
+        private void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
