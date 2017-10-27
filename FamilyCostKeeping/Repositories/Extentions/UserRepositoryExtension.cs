cbabbb05a77302fdffbc587e8069c4d88ea05077
Margarita Smoliakova <msmoliakova@servicechannel.com>
2017-10-27 18:48:41 +0300

    example of repo ext was added

---
+++ FamilyCostKeeping/Repositories/Extentions/UserRepositoryExtension.cs
@@ -0,0 +1,20 @@
+﻿using FamilyCostKeeping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyCostKeeping.Repositories.Extentions
+{
+    public static class UserRepositoryExtension
+    {
+        public static void GetMethodForExample(this IBaseRepository<User> repository, int integer, string st)
+        {
+            // Do query or something here and return the result
+
+            // The result is the following code:
+            // unitOfWork.UserRepository.GetMethodForExample(1, "foo");
+
+        }
+    }
+}
